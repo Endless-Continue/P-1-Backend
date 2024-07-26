5b50a5e628c46afaa4f6712942e0fae0ef3e2ce6
Endless-Continue <javiertorresarrieta01@gmail.com>
2024-07-25 19:59:44 -0500

    X.e-4.0

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import "./conexionDB.js";
 import servidor from "./servidor.js";
 
 //Prender servidor en la ruta 3000
-servidor.listen(2444, () => {
-  console.log("server runing in port 2444");
+servidor.listen(3000, () => {
+  console.log("server runing in port 3000");
 });
